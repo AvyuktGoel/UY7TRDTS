7c24e71b8b8c9adf111a78a011cd46ec25f2a298
AvyuktGoel <71656605+AvyuktGoel@users.noreply.github.com>
2024-03-17 11:25:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCv7OlgmkSTvjSt2rELoVpDy3csDb0F6eE",
+  authDomain: "eride-345dd.firebaseapp.com",
+  projectId: "eride-345dd",
+  storageBucket: "eride-345dd.appspot.com",
+  messagingSenderId: "673161184699",
+  appId: "1:673161184699:web:b5178b86933d6187fa709e"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
